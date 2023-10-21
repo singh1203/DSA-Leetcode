4d2f0462a4c9d0e5cefae731631bd95239c6e58f
Saurabh <singh1203.ss@gmail.com>
2023-10-21 19:38:57 +0530

    Time: 55 ms (62.06%), Space: 43 MB (38.79%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let start = 0;
+    let end = numbers.length - 1;
+    while (start <= end) {
+        if(numbers[start] + numbers[end] === target) return [start + 1, end + 1];
+        if(numbers[start] + numbers[end] > target) {
+            end--;
+        } else {
+            start++;
+        }
+    }
+    return [];
+};
