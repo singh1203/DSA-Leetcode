a05d35d41250e919505f01d911cf248f871a22d1
Saurabh <singh1203.ss@gmail.com>
2023-10-26 21:38:58 +0530

    Time: 66 ms (25.37%), Space: 46.7 MB (13.51%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let distance = Number.MAX_SAFE_INTEGER;
+    let left = 0;
+    let sum = 0;
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right];
+        while(sum >= target) {
+            distance = Math.min(distance, right - left + 1);
+            sum -= nums[left];
+            left++;
+        }
+    }
+    return distance === Number.MAX_SAFE_INTEGER ? 0 : distance;
+};
